e4b747d6a33ff66abb51440302aa0b14d58b0de2
BenHerbst <ben.herbst@web.de>
2022-01-22 23:56:42 +0100

    Init

---
+++ desktop/src/main/kotlin/org/chatfx/desktop/Main.kt
@@ -0,0 +1,7 @@
+package org.chatfx.desktop
+
+import javafx.application.Application
+import javafx.stage.Stage
+import tornadofx.*
+
+class Main: App(MainView::class)
---
+++ desktop/src/main/kotlin/org/chatfx/desktop/MainView.kt
@@ -0,0 +1,7 @@
+package org.chatfx.desktop
+
+import tornadofx.*
+
+class MainView : View() {
+	override val root = label("Gradle + Tornadofx + Jdk 11 in Eclipse!")
+}
