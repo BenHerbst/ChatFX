92beaf8b523cd5db5d18580891a1ebfb446384e5
BenHerbst <ben.herbst@web.de>
2022-01-24 19:09:49 +0100

    Add quit

--- core/src/main/java/core/Query.java
+++ core/src/main/java/core/Query.java
@@ -7,8 +7,6 @@ import core.operations.Browser;
 
 public class Query {
 	public static Conservation takeQuery(String query) {
-		// Take query of the given command / text
-		String[] commands = query.split(" ");
 		// Create the user chatbox
 		ChatboxModel userChatbox = new ChatboxModel();
 		userChatbox.transmitter = Transmitter.USER;
@@ -16,28 +14,35 @@ public class Query {
 		// Create the bot chatbox
 		ChatboxModel botChatbox = new ChatboxModel();
 		botChatbox.transmitter = Transmitter.BOT;
-		switch (commands[0]) {
-		case "open":
-			switch (commands[1]) {
-			case "duckduckgo":
-				Browser.openWithLink("https://duckduckgo.com");
-				botChatbox.text = "Opening duckduckgo.com ...";
+		if (query.toLowerCase().equals("quit")) {
+			// Quit the application
+			System.exit(0);
+		} else {
+			// Take query of the given command / text
+			String[] commands = query.split(" ");
+			switch (commands[0]) {
+			case "open":
+				switch (commands[1]) {
+				case "duckduckgo":
+					Browser.openWithLink("https://duckduckgo.com");
+					botChatbox.text = "Opening duckduckgo.com ...";
+					break;
+				case "google":
+					Browser.openWithLink("https://google.com");
+					botChatbox.text = "Opening google.com ...";
+					break;
+				case "browser":
+					Browser.openWithLink("https://github.com/BenHerbst/ChatFX");
+					botChatbox.text = "Opening the browser ...";
+					break;
+				}
 				break;
-			case "google":
-				Browser.openWithLink("https://google.com");
-				botChatbox.text = "Opening google.com ...";
-				break;
-			case "browser":
-				Browser.openWithLink("https://github.com/BenHerbst/ChatFX");
-				botChatbox.text = "Opening the browser ...";
+			case "search":
+				Browser.searchWithDDG(query.replace("search", ""));
+				botChatbox.text = "Searching via duckduckgo ...";
 				break;
 			}
-			break;
-		case "search":
-			Browser.searchWithDDG(query.replace("search", ""));
-			botChatbox.text = "Searching via duckduckgo ...";
-			break;
+			return new Conservation(userChatbox, botChatbox);
 		}
-		return new Conservation(userChatbox, botChatbox);
 	}
 }
